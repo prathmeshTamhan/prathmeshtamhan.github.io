fe090b575888097eccd231266ef586132733f728
prathmeshTamhan <78070870+prathmeshTamhan@users.noreply.github.com>
2021-03-22 22:25:27 +0530

    Update main.js

--- myportfolio1/myportfolio/assets/js/main.js
+++ myportfolio1/myportfolio/assets/js/main.js
@@ -163,9 +163,9 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
+      typeSpeed: 300,
+      backSpeed: 60,
+      backDelay: 1000
     });
   }
 
